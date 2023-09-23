765752593006b8cdc22d3fabcd40b79c68ef1e42
Francisco Lopez <panchocdk@hotmail.com>
2023-09-23 16:15:50 -0300

    cambios

--- app/routes/usuario_bp.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-from ..controllers.usuario_controller import UserController
-usuario_bp = Blueprint('usuario_bp', __name__)
-usuario_bp.route('/login', methods=['POST'])(UserController.login)
-usuario_bp.route('/profile', methods=['GET'])(UserController.show_profile)
-usuario_bp.route('/logout', methods=['GET'])(UserController.logout)
