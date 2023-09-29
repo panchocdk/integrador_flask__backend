a63ed112a4ca537db83046a3a54c021332c6309a
Francisco Lopez <panchocdk@hotmail.com>
2023-09-29 06:02:40 -0300

    script de carga de datos necesarios

---
+++ Discord Chat Database - load the necesary data.sql
@@ -0,0 +1,8 @@
+USE discord_chat;
+
+INSERT INTO user_roles (role_name) VALUES ('user');
+INSERT INTO user_roles (role_name) VALUES ('admin');
+
+INSERT INTO user_status (status_name) VALUES ('active');
+INSERT INTO user_status (status_name) VALUES ('deactivated');
+INSERT INTO user_status (status_name) VALUES ('suspended');
